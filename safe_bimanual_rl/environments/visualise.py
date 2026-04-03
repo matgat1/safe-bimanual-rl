50c51beaeb185ab5007740a467d0af9bdfb75684
matgat1 <59539017+matgat1@users.noreply.github.com>
2026-04-03 16:12:33 +0200

    Update XML path in visualise.py to use scene.xml

--- safe_bimanual_rl/environments/visualise.py
+++ safe_bimanual_rl/environments/visualise.py
@@ -4,7 +4,7 @@ import mujoco
 import mujoco.viewer
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-xml_path = os.path.join(BASE_DIR, "data", "dual_arm_iiwa_mujoco.xml")
+xml_path = os.path.join(BASE_DIR, "data", "scene.xml")
 
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
