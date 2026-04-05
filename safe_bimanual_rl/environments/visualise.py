eaf224ecc932d1790f8db5d94dd18000ec26a714
matgat1 <59539017+matgat1@users.noreply.github.com>
2026-04-05 18:55:54 +0200

    Update XML path to reference arms_tray_scene.xml

--- safe_bimanual_rl/environments/visualise.py
+++ safe_bimanual_rl/environments/visualise.py
@@ -4,7 +4,7 @@ import mujoco
 import mujoco.viewer
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-xml_path = os.path.join(BASE_DIR, "data", "scene.xml")
+xml_path = os.path.join(BASE_DIR, "data", "arms_tray_scene.xml")
 
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
