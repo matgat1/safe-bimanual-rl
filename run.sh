ecc03a6e6fa1f0f2549700cd27d11049d15f9bf6
matgat1 <59539017+matgat1@users.noreply.github.com>
2026-04-13 17:45:14 +0200

    Add run script for executing reach point experiment in the cluster

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -t 00:10:00
+#SBATCH -J reachCube
+#SBATCH -o output_%j.log
+#SBATCH -e error_%j.err
+
+module load Anaconda3
+source config_conda.sh
+conda activate safe_bimanual_rl
+
+# Aller dans le bon dossier
+cd ~/safe-bimanual-rl
+
+# Lancer l'expérience
+python3 -m safe_bimanual_rl.reach_point_experiment
