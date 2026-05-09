8de0ff2254703aa1265c5af6ccb739e2a5a4bec2
matgat1 <59539017+matgat1@users.noreply.github.com>
2026-05-09 13:13:52 +0200

    Add initial grasp states data for tray pickup environment

--- safe_bimanual_rl/environments/tray_pickup_grasp_env.py
+++ safe_bimanual_rl/environments/tray_pickup_grasp_env.py
@@ -1,5 +1,8 @@
-from safe_bimanual_rl.environments.tray_pickup_base_env import TrayPickUpBaseEnv
+import os
+import mujoco
 import numpy as np
+from safe_bimanual_rl.environments.tray_pickup_base_env import TrayPickUpBaseEnv
+
 
 class TrayPickUpGraspEnv(TrayPickUpBaseEnv):
     """
@@ -7,8 +10,21 @@ class TrayPickUpGraspEnv(TrayPickUpBaseEnv):
     Episodes start with grippers already positioned at the handles (reach phase complete).
     """
 
-    def __init__(self, keyframe: str = "grasp_start", **kwargs):
-        super().__init__(keyframe=keyframe, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.init_states_path = os.path.join(
+            os.path.dirname(__file__), "data", "initial_states","grasp_init_states.npz"
+        )
+        data = np.load(self.init_states_path)
+        self._init_states = list(zip(data["qpos"], data["qvel"], data["act"]))
+
+    def setup(self, obs):
+        super().setup(obs)
+        qpos, qvel, act = self._init_states[np.random.randint(len(self._init_states))]
+        self._data.qpos[:] = qpos
+        self._data.qvel[:] = qvel
+        self._data.act[:] = act
+        mujoco.mj_forward(self._model, self._data)
 
     def reward(self, obs, action, next_obs, absorbing):
         return 0
