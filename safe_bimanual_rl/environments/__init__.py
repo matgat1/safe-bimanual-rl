e25808ec00bfa1cd381f9f5f541e0e57ba9c6e3c
matgat1 <59539017+matgat1@users.noreply.github.com>
2026-05-19 10:19:01 +0200

    Remove unused TrayPickUpLiftEnv import for cleaner environment module

--- safe_bimanual_rl/environments/__init__.py
+++ safe_bimanual_rl/environments/__init__.py
@@ -1,6 +1,5 @@
 from .tray_pickup_reach_env import TrayPickUpReachEnv  # noqa: F401
 from .tray_pickup_grasp_env import TrayPickUpGraspEnv  # noqa: F401
-from .tray_pickup_lift_env import TrayPickUpLiftEnv  # noqa: F401
 from .tray_pickup_base_env import TrayPickUpBaseEnv  # noqa: F401
 from .reach_env import ReachEnv  # noqa: F401
 from .bimanual_table_env import BimanualTableEnv  # noqa: F401
